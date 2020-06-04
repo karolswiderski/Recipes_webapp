188afffc80860ee567bed902d4f1cb944e68f73a
karolswiderski <kswiderski98@gmail.com>
2020-06-04 23:22:14 +0200

    working with a change of category

---
+++ recipes_webapp/Controllers/CategoriesController.cs
@@ -0,0 +1,68 @@
+﻿using recipes_webapp.Models.Data;
+using recipes_webapp.Models.ViewModels.Dishes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace recipes_webapp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories/All/
+        [HttpGet]
+        public ActionResult All(string categoryName, bool flag = false)
+        {
+            List<CategoriesVM> CategoriesList;
+            if (flag == false)
+            {
+                TempData["categoryName"] = "all";
+            }
+            else
+            {
+                TempData["categoryName"] = categoryName;
+            }
+            
+            using(Db db = new Db())
+            {
+                CategoriesList = db.Categories.ToArray().Select(x => new CategoriesVM(x)).ToList();
+            }
+            return View(CategoriesList);
+        }
+        
+
+        [HttpGet]
+        public ActionResult Recipes(string searching, string name)
+        {
+            List<DishesVM> DishesList;
+
+            if (string.IsNullOrEmpty(searching))
+            {
+                using (Db db = new Db())
+                {
+                    if (name == "all") DishesList = db.Dishes.ToArray().Select(x => new DishesVM(x)).ToList();
+                    else
+                    {
+                        CategoriesDTO category = db.Categories.FirstOrDefault(x => x.Name == name);
+                        DishesList = db.Dishes.Where(x => x.Id_Category == category.Id_Category).ToArray().Select(x => new DishesVM(x)).ToList();
+                    }
+                }
+            }
+            else
+            {
+                using (Db db = new Db())
+                {
+                    if (name == "all") DishesList = db.Dishes.ToArray().Where(x => x.Name.StartsWith(searching) || searching == null).Select(x => new DishesVM(x)).ToList();
+                    else
+                    {
+                        CategoriesDTO category = db.Categories.FirstOrDefault(x => x.Name == name);
+                        DishesList = db.Dishes.ToArray().Where(x => x.Name.StartsWith(searching) || searching == null && x.Id_Category == category.Id_Category).Select(x => new DishesVM(x)).ToList();
+                    }
+                }
+            }
+
+            return PartialView(DishesList);
+        }
+    }
+}
--- recipes_webapp/Controllers/RecipesController.cs
+++ recipes_webapp/Controllers/RecipesController.cs
@@ -10,41 +10,62 @@ namespace recipes_webapp.Controllers
 {
     public class RecipesController : Controller
     {
-        // GET: Recipes
+        // GET: Recipes/
         public ActionResult Index(string searching)
         {
+            //Console.WriteLine("searching: " + searching);
+            //Console.WriteLine("categoryName: " + categoryName);
+            if (TempData["categoryName"].ToString() != null) {
+            
+}
+            string categoryName = TempData["categoryName"].ToString();
             List<DishesVM> DishesList;
 
             if (string.IsNullOrEmpty(searching))
             {
                 using (Db db = new Db())
                 {
-                    DishesList = db.Dishes.ToArray().Select(x => new DishesVM(x)).ToList();
+                    if (categoryName == null) DishesList = db.Dishes.ToArray().Select(x => new DishesVM(x)).ToList();
+                    else
+                    {
+                        CategoriesDTO category = db.Categories.FirstOrDefault(x => x.Name == categoryName);
+                        DishesList = db.Dishes.Where(x => x.Id_Category == category.Id_Category).ToArray().Select(x => new DishesVM(x)).ToList();
+                    }
                 }
             }
             else {
                 using (Db db = new Db())
                 {
-                    DishesList = db.Dishes.ToArray().Where(x => x.Name.StartsWith(searching) || searching == null).Select(x => new DishesVM(x)).ToList();
+                    if (categoryName == null) DishesList = db.Dishes.ToArray().Where(x => x.Name.StartsWith(searching) || searching == null).Select(x => new DishesVM(x)).ToList();
+                    else
+                    {
+                        CategoriesDTO category = db.Categories.FirstOrDefault(x => x.Name == categoryName);
+                        DishesList = db.Dishes.ToArray().Where(x => x.Name.StartsWith(searching) || searching == null && x.Id_Category == category.Id_Category).Select(x => new DishesVM(x)).ToList();
+                    } 
                 }
             }
 
             return View(DishesList);
         }
 
-        // GET: recipes/aa
-        public ActionResult aa()
+        // GET: Recipes/Categories/name
+        public void Categories(string name)
         {
-            List<CategoriesVM> l;
-
-            using (Db db = new Db())
-            {
-                l = db.Categories.ToArray().Select(x => new CategoriesVM(x)).ToList();
-            }
-
-            return View(l);
+            // return RedirectToAction("Index", new { searching = "", categoryName = name});
+            TempData["categoryName"] = name;
         }
 
+        public ActionResult ListOfCategories() {
+            List<CategoriesVM> CategoriesList;
+            using (Db db = new Db())
+            {
+                CategoriesList = db.Categories.ToArray().Select(x => new CategoriesVM(x)).ToList();
+            }
+            return PartialView(CategoriesList);
+        }
+
+        
+        
     }
 
 }
