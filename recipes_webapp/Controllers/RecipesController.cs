8bc120a51323ed035ffde08a9fe201a37c70fe5e
karolswiderski <kswiderski98@gmail.com>
2020-06-10 20:21:04 +0200

    add: RecipesController and start working with Recipe Details

---
+++ recipes_webapp/Controllers/RecipesController.cs
@@ -0,0 +1,35 @@
+﻿using recipes_webapp.Models.Data;
+using recipes_webapp.Models.ViewModels.Dishes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace recipes_webapp.Controllers
+{
+    public class RecipesController : Controller
+    {
+        // GET: Recipes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Recipes/Details
+        public ActionResult Details(int id)
+        {
+            DishesDTO recipeDTO;
+            DishesVM recipeVM;
+
+            using (Db db = new Db())
+            {
+                recipeDTO = db.Dishes.Where(x => x.Id_Dish == id).FirstOrDefault();
+                recipeVM = new DishesVM(recipeDTO);
+            }
+
+
+           return View(recipeVM);
+        }
+    }
+}
