ae1da65d121c46e34e44b0b3a585c5150ea755e1
karolswiderski <kswiderski98@gmail.com>
2020-06-17 17:35:16 +0200

    start working with Home Page

---
+++ recipes_webapp/Scripts/MyScripts/Recipes/Photo-Slider.js
@@ -0,0 +1,23 @@
+﻿var index = 1;
+
+
+function showSlides(x, flag) {
+
+    document.getElementById("photo" + index).style.display = "none";
+
+    if (flag == true) {
+        if (index > 0 && index < 5) {
+            index++;
+        } else if (index == 5) {
+            index = 1;
+        }
+    } else {
+        if (index > 1 && index < 6) {
+            index--;
+        } else if (index == 1) {
+            index = 5;
+        }
+    }
+
+    document.getElementById("photo" + index).style.display = "block";
+}
