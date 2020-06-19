2a20b19f728f9aa9eec68312f29772d5100e2a28
karolswiderski <kswiderski98@gmail.com>
2020-06-19 23:19:32 +0200

    update Home Page

---
+++ recipes_webapp/Controllers/ArticlesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace recipes_webapp.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        // GET: Articles
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult BestArticle() {
+
+            return PartialView();
+        }
+    }
+}
---
+++ recipes_webapp/Scripts/MyScripts/Recipes/Automatic-Photo-Slider.js
@@ -0,0 +1,31 @@
+﻿var index = 1;
+showSlides();
+
+function showSlides() {
+
+    $("#photo" + index).fadeOut(800);
+   // var dot1 = document.getElementById("#dot" + index);
+    //dot1.classList.add("active");
+
+    if (index == 1) $("#d5").removeClass("active");
+    else $("#d" + (index - 1)).removeClass("active");
+
+
+    $("#d" + index).addClass("active");
+
+    console.log("asdasd");
+        if (index > 0 && index < 5) {
+            index++;
+        } else if (index == 5) {
+            index = 1;
+        }
+    
+
+    setTimeout(function () {
+        $("#photo" + index).fadeIn(1000);
+       // var dot2 = document.getElementById("#dot" + index);
+        //dot2.classList.remove("active");
+    }, 800);
+
+    setTimeout(showSlides, 8000);
+}
