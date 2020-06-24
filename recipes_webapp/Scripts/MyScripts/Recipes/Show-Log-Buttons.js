35bf2bf32bdfeed0d6cea3641e38dc05457cdf13
karolswiderski <kswiderski98@gmail.com>
2020-06-24 20:56:39 +0200

    add: Animated button operation in Home View

---
+++ recipes_webapp/Scripts/MyScripts/Recipes/Show-Log-Buttons.js
@@ -0,0 +1,9 @@
+﻿function showTxt(x) {
+        $(".text" + x).css("display", "inline");
+        $(".icn" + x).css("color", "white");
+}
+
+function hideTxt(x) {
+    $(".text" + x).css("display", "none");
+    $(".icn" + x).css("color", "#3e4444");
+}
