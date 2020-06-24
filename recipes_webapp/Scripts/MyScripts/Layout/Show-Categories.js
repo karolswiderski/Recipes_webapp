2e98178641b42e6f047f6e113bb777f7670e74d0
karolswiderski <kswiderski98@gmail.com>
2020-06-24 22:00:43 +0200

    a little update in top menu bar

--- recipes_webapp/Scripts/MyScripts/Layout/Show-Categories.js
+++ recipes_webapp/Scripts/MyScripts/Layout/Show-Categories.js
@@ -1,9 +1,13 @@
 ﻿function openCategories() {
     $("#categories-box").fadeIn(300);
-    $(".cat").css( "color","RGB(136, 176, 75)" );
+    $(".cat").css("color", "RGB(136, 176, 75)");
+    $("#caticonid").removeClass("fas fa-caret-down");
+    $("#caticonid").addClass("fas fa-caret-up");
 }
 
 function closeCategories() {
     $("#categories-box").fadeOut(300);
     $(".cat").css("color", "white");
+    $("#caticonid").removeClass("fas fa-caret-up");
+    $("#caticonid").addClass("fas fa-caret-down");
 }
