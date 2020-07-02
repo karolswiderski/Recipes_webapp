d1fc483600edfc2c3199f6f302c87929fe60bd85
karolswiderski <kswiderski98@gmail.com>
2020-07-02 21:08:06 +0200

    added script to change view in MyAccount

---
+++ recipes_webapp/Scripts/MyScripts/Account/MyAccount-Select-Options.js
@@ -0,0 +1,12 @@
+﻿selectOption(1);
+
+function selectOption(x) {
+    for (var i = 1; i < 6; i++) {
+        document.getElementsByClassName('box' + i)[0].style.display = 'none';
+        document.getElementsByClassName('box-button-' + i)[0].style.fontWeight = 'normal';
+        document.getElementsByClassName('box-button-' + i)[0].style.color = '#bfbfbf';
+    }
+    document.getElementsByClassName('box' + x)[0].style.display = 'block';
+    document.getElementsByClassName('box-button-' + x)[0].style.fontWeight = 'bold';
+    document.getElementsByClassName('box-button-' + x)[0].style.color = '#ffffff';
+}
